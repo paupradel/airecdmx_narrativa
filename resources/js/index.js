50865fce6dc1bf689bde785adf9a8a3437975e4b
Paulina Pradel <pradel.paulina@ciencias.unam.mx>
2019-10-02 19:43:01 -0500

    funciones de entrada y salida para los párrafos

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,11 +3,18 @@ var scroller = scrollama();
 
 function handleStepEnter(response) {
     response.element.style.opacity=1;
+    document.body.style.backgroundImage='url(data-step)';
+}
+
+function handleStepExit(response) {
+    response.element.style.opacity=0.4;
 }
 
 scroller
     .setup({
         step: ".step",
         debug: true,
+        offset: 0.5,
     })
     .onStepEnter(handleStepEnter)
+    .onStepExit(handleStepExit);
