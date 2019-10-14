f56e5ef4e7c4db19feb4c4ca177ddd809d47744d
Paulina Pradel <pradel.paulina@ciencias.unam.mx>
2019-10-14 18:39:01 -0500

    formato de números

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,13 +1,6 @@
 
 var scroller = scrollama();
 
-// function handleStepEnter(response) {
-//     response.element.style.opacity=1;
-//     let img_url=response.element.getAttribute("data-step");
-//     document.body.style.backgroundImage='url('+img_url+'),url("resources/images/background_1.png")';
-//     document.body.style.backgroundBlendMode = "multiply";
-// }
-
 function handleStepEnter(response) {
     response.element.style.opacity=1;
     if (response.element.getAttribute("data-step-1")) {
