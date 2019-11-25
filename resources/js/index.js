31bcadd4a8fff06fc9751474864779a948e96813
paupradel <pradel.paulina@ciencias.unam.mx>
2019-11-25 13:27:26 -0600

    orden de aparición de nubes

--- resources/js/index.js
+++ resources/js/index.js
@@ -5,40 +5,17 @@ function handleStepEnter(response) {
     response.element.style.opacity=1;
     if (response.element.getAttribute("data-step-1")) {
         let img_url = response.element.getAttribute("data-step-1");
-        document.body.style.backgroundImage = 'url(' +img_url+ '), url("resources/images/background_1.png")';
+        document.body.style.backgroundImage = 'url(' +img_url+ '), url("resources/images/background_9_3.png"),' +
+            'url("resources/images/background_9_2.png"), url("resources/images/background_9_1.png"),' +
+            'url("resources/images/background_1.png")';
         document.body.style.backgroundBlendMode= "normal";
-
     }
 
     else if (response.element.getAttribute("data-step-2")) {
         let img_url = response.element.getAttribute("data-step-2");
-        document.body.style.backgroundImage = 'url(' +img_url+ '), url("resources/images/background_9_1.png"),' +
-            ' url("resources/images/background_1.png")';
-        document.body.style.backgroundBlendMode= "normal";
-
-    }
-
-    else if (response.element.getAttribute("data-step-3")) {
-        let img_url = response.element.getAttribute("data-step-3");
-        document.body.style.backgroundImage = 'url(' +img_url+ '),url("resources/images/background_9_2.png"), ' +
-            'url("resources/images/background_9_1.png"), url("resources/images/background_1.png")';
-        document.body.style.backgroundBlendMode= "normal";
-    }
-
-    else if (response.element.getAttribute("data-step-4")) {
-        let img_url = response.element.getAttribute("data-step-4");
-        document.body.style.backgroundImage = 'url(' +img_url+ '), url("resources/images/background_9_3.png"), ' +
-            'url("resources/images/background_9_2.png"), url("resources/images/background_9_1.png"), ' +
-            'url("resources/images/background_1.png")';
-        document.body.style.backgroundBlendMode= "normal";
-
-    }
-
-    else if (response.element.getAttribute("data-step-5")) {
-        let img_url = response.element.getAttribute("data-step-5");
-        document.body.style.backgroundImage = 'url(' + img_url + '), url("resources/images/lineas_9.png"),' +
+        document.body.style.backgroundImage =  'url(' + img_url + '), url("resources/images/lineas_9.png"),' +
             'url("resources/images/background_9_4.png"),url("resources/images/background_9_3.png")' +
-            ',url("resources/images/background_9_2.png"), url("resources/images/background_9_1.png"), ' +
+            ',url("resources/images/background_9_2.png"), url("resources/images/background_9_1.png"),' +
             'url("resources/images/background_1.png")';
         document.body.style.backgroundBlendMode= "normal";
     }
